927fcad6a97e2a0857421ee14d37f6e3b3e5dd2f
Ahmadalhomsi <tarikuka.aa@gmail.com>
2025-01-20 14:25:25 +0300

    Prisma Instiance

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  // In development, we don't want to create a new client every time hot reload happens
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
