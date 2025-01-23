c0dfde4d2aacedde7db25c3e15efe5d533aba9c2
Ahmadalhomsi <tarikuka.aa@gmail.com>
2025-01-23 14:33:02 +0300

    Removing the email and password for validation endpoint

--- app/api/external/validation/route.js
+++ app/api/external/validation/route.js
@@ -3,34 +3,26 @@ import prisma from '@/lib/prisma';  // Import the prisma instance from the file
 
 export async function POST(request) {
     const data = await request.json();
-    const { email, password, token } = data;
+    const { token } = data;
     try {
-        const customer = await prisma.customer.findUnique({
-            where: { email: email, password: password },
+
+        const newService = await prisma.service.findUnique({
+            where: { id: token },
         });
 
-        if (!customer) {
-            console.log("Customer not found");
+        if (!newService) {
+            console.log("Service not found");
             return NextResponse.json({ valid: false }, { status: 404 });
         } else {
-            const newService = await prisma.service.findUnique({
-                where: { id: token },
-            });
+            const today = new Date();
+            const endDate = new Date(newService.endingDate);
 
-            if (!newService) {
-                console.log("Service not found");
-                return NextResponse.json({ valid: false }, { status: 404 });
-            } else {
-                const today = new Date();
-                const endDate = new Date(newService.endingDate);
-
-                if (endDate <= today) { // Service end date is today or in the past
-                    console.log("Service has already ended");
-                    return NextResponse.json({ valid: false, endingDate: newService.endingDate }, { status: 400 });
-                }
-                else {
-                    return NextResponse.json({ valid: true, endingDate: newService.endingDate }, { status: 200 });
-                }
+            if (endDate <= today) { // Service end date is today or in the past
+                console.log("Service has already ended");
+                return NextResponse.json({ valid: false, endingDate: newService.endingDate }, { status: 400 });
+            }
+            else {
+                return NextResponse.json({ valid: true, endingDate: newService.endingDate }, { status: 200 });
             }
         }
     } catch (error) {
