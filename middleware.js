bca8d4f967a8c66bcc906254e85acfa4d393bb20
Ahmad Alhomsi <tarikuka.aa@gmail.com>
2026-02-25 10:56:02 +0300

    Allowing access for cron

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ export async function middleware(req) {
   const { pathname } = req.nextUrl;
 
   // Allow public routes
-  const publicRoutes = ["/login", "/api/login", "/api/external", "/admins"];
+  const publicRoutes = ["/login", "/api/login", "/api/external", "/admins", "/api/cron"];
   if (publicRoutes.some((route) => pathname.startsWith(route))) {
     return NextResponse.next();
   }
